9e8e8b1e8d93aba8de579f55b4f05cd262f6ed9c
Kostian <kostia.talamaniuk@gmail.com>
2025-03-04 22:34:54 +0000

    Task4

---
+++ task4.py
@@ -0,0 +1,25 @@
+import datetime
+
+def get_upcoming_birthdays(users):
+
+    today = datetime.date.today()
+    upcoming_birthdays = []
+
+    for user in users:
+        birth_date = datetime.datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+        birthday_this_year = birth_date.replace(year=today.year)
+
+        if birthday_this_year < today:
+            birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+
+        days_until_birthday = (birthday_this_year - today).days
+        if 0 <= days_until_birthday <= 7:
+            if birthday_this_year.weekday() in [5, 6]:  # 5 = субота, 6 = неділя
+                birthday_this_year += datetime.timedelta(days=(7 - birthday_this_year.weekday()))
+
+            upcoming_birthdays.append({
+                "name": user["name"],
+                "congratulation_date": birthday_this_year.strftime("%Y.%m.%d")
+            })
+
+    return upcoming_birthdays
