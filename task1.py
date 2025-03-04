45bde4257e1353314a713958b3a2ec197db11544
Kostian <kostia.talamaniuk@gmail.com>
2025-03-04 22:14:36 +0000

    Task1

---
+++ task1.py
@@ -0,0 +1,11 @@
+import datetime
+def get_days_from_today(date_str: str) -> int:
+
+    try:
+        given_date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+    except ValueError as e:
+        raise ValueError("Невірний формат дати. Використовуйте формат 'РРРР-ММ-ДД'") from e
+
+    today = datetime.date.today()
+    days_difference = (today - given_date).days
+    return days_difference
